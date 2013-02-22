f472c83cb229b8c43ea23b9e92648eb04583ab77
Timur Vafin <timur.vafin@flatstack.com>
2013-02-22 14:22:17 +0400

    Init rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gifme::Application.config.secret_token = '41ae0bdf14c1956780ba13531ec456d297f7bf8bc62837b35db1f9f0466b45b3f6f99010974795be4a6e6fe9dd19a5fb97293e983725afc925badb2025281743'
